6afa827e1eff56db524505ffd8498b80ab2495c7
alexandre <alexandre@localhost.localdomain>
2014-04-02 00:05:26 -0300

    Versão 1.0.2 Pagina inicial Signed-off-by:alexandre <alexandre@localhost.localdomain>

--- application/views/home.php
+++ application/views/home.php
@@ -1,14 +1,47 @@
 <!DOCTYPE html>
-<html lang="pt-br"> <!-- Idioma da pagina. -->
+<!--[if lt IE 7 ]><html class="ie ie6" lang="pt-br"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="pt-br"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="pt-br"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
+    <?php echo doctype('xhtml1-trans'); ?>
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <html lang="pt-br">
+        <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
     <title>Alexandre livre - Desenvolvedor de aplicações Web.</title>
 
+    
+          <!-- Mobile Specific Metas
+    ================================================== -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <?php
+        $meta= array(
+          
+          array('name' => 'robots', 'content' => 'no-cache'),
+          array('name' => 'discription', 'content' => 'Alexandre livre - Templates e web sites'),
+          
+          array('name' => 'keyworks', 'content' => 'sistemas, sistemas web, sistema administrativo web,
+              loja online, desenvolvimento de sites, Templates para blogs, modelos para blogs, , sites,
+              templates para sites, modelos para sites, Templates, consultoria de projetos, manutenção,
+              suporte ao projeto, otimização de sites'),
+            
+          array('name' => 'robots', 'content' => 'no-cache'),
+          array('name' => 'Content-type', 'content' => 'text/html; charset=utf-8', 'type' => 'equiv'),
+          array('name' => 'autor', 'content' => '')
+            
+            );    
+        
+          echo meta($meta);
+     
+          // caregando os icons do site
+          echo link_tag('img/icon/apple-touch-icon-114x114.png', 'apple-touch-icon', 'image/ico');
+          echo link_tag('img/icon/apple-touch-icon-144x144.png', 'apple-touch-icon', 'image/ico');
+          echo link_tag('img/icon/apple-touch-icon-72x72.png', 'apple-touch-icon', 'image/ico');
+     ?>    
+    
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.css" rel="stylesheet">
 
@@ -38,11 +71,11 @@
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse navbar-right navbar-ex1-collapse">
                 <ul class="nav navbar-nav">
-                    <li><a href="#about">About</a>
+                    <li><a href="#Sobre">Sobre</a>
                     </li>
-                    <li><a href="#services">Services</a>
+                    <li><a href="#Vitrine">Vitrine</a>
                     </li>
-                    <li><a href="#contact">Contact</a>
+                    <li><a href="#Contato">Contato</a>
                     </li>
                 </ul>
             </div>
@@ -58,8 +91,8 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="intro-message">
-                        <h1>Landing Page</h1>
-                        <h3>A Template by Start Bootstrap</h3>
+                        <h1>Desenvolvimento de ideias</h1>
+                        <h3>Suporte do modo que você gosta</h3>
                         <hr class="intro-divider">
                         <ul class="list-inline intro-social-buttons">
                             <li><a href="https://twitter.com/alexandrelivre" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
@@ -87,12 +120,12 @@
                 <div class="col-lg-5 col-sm-6">
                     <hr class="section-heading-spacer">
                     <div class="clearfix"></div>
-                    <h2 class="section-heading">Death to the Stock Photo:
-                        <br>Special Thanks</h2>
-                    <p class="lead">A special thanks to Death to the Stock Photo for providing the photographs that you see in this template. <a target="_blank" href="http://join.deathtothestockphoto.com/">Visit their website</a> to become a member.</p>
+                    <h2 class="section-heading">Sistemas elegantes e bonitos.
+                        <br>Algo elegante.</h2>
+                    <p class="lead">Que farram teus concorrentes passar a comprar os teus produtos...&nbsp;&nbsp;<a target="_blank" href="http://www.loja.alexandrelivre.com/">Acesse nossa loja</a>&nbsp;&nbsp;Compre já o seu sistema.</p>
                 </div>
                 <div class="col-lg-5 col-lg-offset-2 col-sm-6">
-                    <img class="img-responsive" src="img/ipad.png" alt="">
+                    <img class="img-responsive" src="img/doge.png" alt="">
                 </div>
             </div>
 
@@ -110,12 +143,11 @@
                 <div class="col-lg-5 col-lg-offset-1 col-sm-push-6  col-sm-6">
                     <hr class="section-heading-spacer">
                     <div class="clearfix"></div>
-                    <h2 class="section-heading">3D Device Mockups
-                        <br>by PSDCovers</h2>
-                    <p class="lead">Turn your 2D designs into high quality, 3D product shots in seconds using free Photoshop actions by PSDCovers! <a target="_blank" href="http://www.psdcovers.com/">Visit their website</a> to download some of their awesome, free photoshop actions!</p>
+                    <h2 class="section-heading">Sistemas para mobile</h2>
+                    <p class="lead">Sistemas do jeito que você precisar, encomende o seu de modo que não precise se preocupar.&nbsp;&nbsp;<a target="_blank" href="#">Faça já o seu pedido</a></p>
                 </div>
                 <div class="col-lg-5 col-sm-pull-6  col-sm-6">
-                    <img class="img-responsive" src="img/doge.png" alt="">
+                    <img class="img-responsive" src="img/ipad.png" alt="">
                 </div>
             </div>
 
@@ -133,9 +165,9 @@
                 <div class="col-lg-5 col-sm-6">
                     <hr class="section-heading-spacer">
                     <div class="clearfix"></div>
-                    <h2 class="section-heading">Google Web Fonts and
-                        <br>Font Awesome Icons</h2>
-                    <p class="lead">This template features the 'Lato' font, part of the <a target="_blank" href="http://www.google.com/fonts">Google Web Font library</a>, as well as <a target="_blank" href="http://fontawesome.io">icons from Font Awesome</a>.</p>
+                    <h2 class="section-heading">Algo agradável, bonito. 
+                        <br>Algo que te lembre a vida.</h2>
+                    <p class="lead">Todos nos achamos que você merece algo muito bonito.&nbsp;Você não irra se arrepender de nos pedir para fazer o projeto dos seus sonhos.</p>
                 </div>
                 <div class="col-lg-5 col-lg-offset-2 col-sm-6">
                     <img class="img-responsive" src="img/phones.png" alt="">
@@ -154,7 +186,7 @@
 
             <div class="row">
                 <div class="col-lg-6">
-                    <h2>Connect to Start Bootstrap:</h2>
+                    <h2>Conecte-se conosco:</h2>
                 </div>
                 <div class="col-lg-6">
                     <ul class="list-inline banner-social-buttons">
@@ -179,19 +211,19 @@
             <div class="row">
                 <div class="col-lg-12">
                     <ul class="list-inline">
-                        <li><a href="#home">Home</a>
+                        <li><a href="#">Inicio</a>
                         </li>
                         <li class="footer-menu-divider">&sdot;</li>
-                        <li><a href="#about">About</a>
+                        <li><a href="#Sobre">Sobre</a>
                         </li>
                         <li class="footer-menu-divider">&sdot;</li>
-                        <li><a href="#services">Services</a>
+                        <li><a href="#Vitrine">Vitrine</a>
                         </li>
                         <li class="footer-menu-divider">&sdot;</li>
-                        <li><a href="#contact">Contact</a>
+                        <li><a href="#Contato">Contato</a>
                         </li>
                     </ul>
-                    <p class="copyright text-muted small">Copyright &copy; Your Company 2014. All Rights Reserved</p>
+                    <p class="copyright text-muted small">Copyright &copy; Alexandrelivre 2014. Todos os direitos reservados</p>
                 </div>
             </div>
         </div>
